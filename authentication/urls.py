89baac3a0f364b0aebed6e0c9dff95899a34d6f9
Nicolas Rondon <nicolasrondon@outlook.es>
2020-03-18 18:08:30 -0500

    ADD Core exceptions

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,5 @@ from .views import RegistrationAPIView, LoginAPIView
 app_name = 'authentication'
 urlpatterns = [
     path('users/', RegistrationAPIView.as_view()),
-    path(r'^users/login/?$', LoginAPIView.as_view()),
+    path('users/login/', LoginAPIView.as_view()),
 ]
--- codesuit/settings.py
+++ codesuit/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     # Local
-    'authentication.apps.AuthenticationConfig'
+    'authentication.apps.AuthenticationConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'authentication.User'
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'core.exceptions.core_exception_handler',
+    'NON_FIELD_ERRORS_KEY': 'error',
+}
