269b6bdf7e8228bc09ceb2d29819aca86ce054e3
Nicolas Rondon <nicolasrondon@outlook.es>
2020-03-18 17:48:45 -0500

    ADD LoginSerializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import User
 
@@ -15,3 +16,37 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
         def create(self, validated_data):
             return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    username = serializers.CharField(max_length=255, read_only=True)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        if email is None:
+            raise serializers.ValidationError(
+                'Es necesario una contraseña para ingresar'
+            )
+
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError(
+                'Parece que el usuario y contraseña son incorrectos'
+            )
+
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'Este usuario ha sido desactivado'
+            )
+
+        return {
+            'email': user.email,
+            'username': user.username,
+            'token': user.token
+        }
