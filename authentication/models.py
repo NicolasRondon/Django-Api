a3eb1e31ca73aba29f3de15702fc584648d8d3a5
Nicolas Rondon <nicolasrondon@outlook.es>
2020-03-18 16:45:02 -0500

    ADD User model

--- authentication/models.py
+++ authentication/models.py
@@ -38,3 +38,39 @@ class UserManager(BaseUserManager):
         user.save()
 
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.CharField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.email
+    
+    @property
+    def token(self):
+        return self._generate_jwt_token()
+    
+    def get_full_name(self):
+        return self.email
+    
+    def get_short_name(self):
+        return self.username
+    
+    def _generate_jwt_token(self):
+        dt = datetime.now() + timedelta(days=60)
+        
+        token = jwt.encode({
+            'id': self.pk,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, algorithm='HS256')
+        
+        return token.decode('utf-8')
