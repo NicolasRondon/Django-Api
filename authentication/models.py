df4a419492caeca40567999d0cfc8ccb08b18a35
Nicolas Rondon <nicolasrondon@outlook.es>
2020-03-19 10:56:45 -0500

    FIX login

--- authentication/models.py
+++ authentication/models.py
@@ -8,16 +8,13 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, Permis
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    """
-    Creando usuario
-    """
-
-    def create_user(self, username, email, password):
+    def create_user(self, username, email, password=None):
+        """Create and return a `User` with an email, username and password."""
         if username is None:
-            raise TypeError('Debe existir un username')
+            raise TypeError('Users must have a username.')
 
         if email is None:
-            raise TypeError('Debe existir un email')
+            raise TypeError('Users must have an email address.')
 
         user = self.model(username=username, email=self.normalize_email(email))
         user.set_password(password)
@@ -26,11 +23,8 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, username, email, password):
-        """
-        Creando Super Usuario
-        """
         if password is None:
-            raise TypeError('Debe existir una contraseña')
+            raise TypeError('Superusers must have a password.')
 
         user = self.create_user(username, email, password)
         user.is_superuser = True
@@ -47,30 +41,31 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    
+
     objects = UserManager()
-    
+
     def __str__(self):
         return self.email
-    
+
     @property
     def token(self):
         return self._generate_jwt_token()
-    
+
     def get_full_name(self):
-        return self.email
-    
+        return self.username
+
     def get_short_name(self):
         return self.username
-    
+
     def _generate_jwt_token(self):
         dt = datetime.now() + timedelta(days=60)
-        
+
         token = jwt.encode({
             'id': self.pk,
             'exp': int(dt.strftime('%s'))
         }, settings.SECRET_KEY, algorithm='HS256')
-        
+
         return token.decode('utf-8')
