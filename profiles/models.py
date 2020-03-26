01ae7509ae50ef8465839a35d3aa643d33cbae16
Nicolas Rondon <nicolasrondon@outlook.es>
2020-03-25 20:26:38 -0500

    READY following

--- profiles/models.py
+++ profiles/models.py
@@ -8,6 +8,10 @@ class Profile(TimestampedModel):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE
     )
+    follows = models.ManyToManyField(
+        'self',
+        related_name='followed_by',
+        symmetrical=False)
 
     bio = models.TextField(blank=True)
 
@@ -15,3 +19,15 @@ class Profile(TimestampedModel):
 
     def __str__(self):
         return self.user.username
+
+    def follow(self, profile):
+        self.follows.add(profile)
+
+    def unfollow(self, profile):
+        self.follows.remove(profile)
+
+    def is_following(self, profile):
+        return self.follows.filter(pk=profile.pk).exists()
+
+    def is_followed_by(self, profile):
+        return self.followed_by.filter(pk=profile.pk).exists()
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from rest_framework.exceptions import NotFound
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 from .models import Profile
 from .renderers import ProfileJSONRenderer
 from .serializers import ProfileSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
 
 
 class ProfileRetrieveAPIView(RetrieveAPIView):
@@ -21,6 +22,49 @@ class ProfileRetrieveAPIView(RetrieveAPIView):
         except Profile.DoesNotExist:
             raise NotFound('Un perfil con este usuario no existe')
 
-        serializer = self.serializer_class(profile)
+        serializer = self.serializer_class(profile, context={
+            'request': request
+        })
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProfileFollowAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (ProfileJSONRenderer,)
+    serializer_class = ProfileSerializer
+
+    def delete(self, request, username=None):
+        follower = self.request.user.profile
+
+        try:
+            followee = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise NotFound('No se encontro un perfil para este usuario')
+
+        follower.unfollow(followee)
+
+        serializer = self.serializer_class(followee, context={
+            'request': request
+        })
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, username=None):
+        follower = self.request.user.profile
+
+        try:
+            followee = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise NotFound('No se encontro un perfil para este usuario')
+
+        if follower.pk is followee.pk:
+            raise serializers.ValidationError('No puedes seguirte a ti mismo.')
+
+        follower.follow(followee)
+
+        serializer = self.serializer_class(followee, context={
+            'request': request
+        })
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
