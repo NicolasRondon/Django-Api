9910de4bd0871d7d1428624d42051dd5fc7924d3
Nicolas Rondon <nicolasrondon@outlook.es>
2020-03-25 20:02:29 -0500

    READY Comments

--- articles/renderers.py
+++ articles/renderers.py
@@ -4,3 +4,8 @@ from core.renderers import CodesuitJSONRenderer
 class ArticleJSONRenderer(CodesuitJSONRenderer):
     object_label = 'article'
     object_label_plural = 'articles'
+
+
+class CommentJSONRenderer(CodesuitJSONRenderer):
+    object_label = 'comment'
+    object_label_plural = 'comments'
--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import ArticleViewSet
+from .views import ArticleViewSet, CommentsListCreateAPIView, CommentsDestroyAPIView
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'articles', ArticleViewSet)
@@ -10,4 +10,12 @@ router.register(r'articles', ArticleViewSet)
 app_name = 'articles'
 urlpatterns = [
     path('', include(router.urls)),
+    path(
+        'articles/<slug:article_slug>/comments',
+        CommentsListCreateAPIView.as_view()
+        ),
+    path(
+        'articles/<slug:article_slug>/comments/<int:comment_pk>',
+        CommentsDestroyAPIView.as_view()
+        ),
 ]
