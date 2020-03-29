ebd1ef81c0e2d0bfd71fbed0db81bc9d47ee1c51
Nicolas <nicolasrondon@outlook.es>
2020-03-28 22:10:22 -0500

    READY Filter

--- articles/urls.py
+++ articles/urls.py
@@ -2,14 +2,15 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import ArticleViewSet, CommentsListCreateAPIView, CommentsDestroyAPIView, ArticlesFavoriteAPIView, TagListAPIView
+from .views import ArticleViewSet, CommentsListCreateAPIView, CommentsDestroyAPIView, ArticlesFavoriteAPIView, \
+    TagListAPIView, ArticlesFeedAPIView
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'articles', ArticleViewSet)
 
 app_name = 'articles'
 urlpatterns = [
-    path('', include(router.urls)),
+    path('articles/feed', ArticlesFeedAPIView.as_view()),
     path(
         'articles/<slug:article_slug>/comments',
         CommentsListCreateAPIView.as_view()
@@ -21,4 +22,5 @@ urlpatterns = [
     path('articles/<slug:article_slug>/favorite',
          ArticlesFavoriteAPIView.as_view()),
     path('tags', TagListAPIView.as_view()),
+    path('', include(router.urls)),
 ]
